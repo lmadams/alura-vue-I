71ba7bfd94ef2fcc89568df223478ed1a5a5112d
Lucas Adams <adams.lucass@gmail.com>
2017-05-31 23:25:39 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1 v-text="titulo"></h1>
+
+    <ul>
+      <li v-for="foto of fotos">
+        <img v-bind:src="foto.url" v-bind:alt="foto.titulo">
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      titulo: 'Alurapic',
+      fotos: [
+        {
+          url: 'http://www.maedecachorro.com.br/wp-content/uploads/2017/01/cachorro-foto-de-divulga%C3%A7%C3%A3o.jpg',
+          titulo: 'Dog'
+        },
+        {
+          url: 'http://www.maedecachorro.com.br/wp-content/uploads/2017/01/cachorro-foto-de-divulga%C3%A7%C3%A3o.jpg',
+          titulo: 'Dogao'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
