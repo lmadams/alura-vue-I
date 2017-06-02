953acbfec963473ecf5dbfea01ae271472038d8c
Lucas Adams <adams.lucass@gmail.com>
2017-06-01 21:38:22 -0300

    modulo 2

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,28 @@
 <template>
-  <div>
-    <h1 v-text="titulo"></h1>
+    <div>
+        <h1 v-text="titulo"></h1>
 
-    <ul>
-      <li v-for="foto of fotos">
-        <img v-bind:src="foto.url" v-bind:alt="foto.titulo">
-      </li>
-    </ul>
-  </div>
+        <ul>
+            <li v-for="foto of fotos">
+                <img v-bind:src="foto.url" v-bind:alt="foto.titulo">
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      titulo: 'Alurapic',
-      fotos: [
-        {
-          url: 'http://www.maedecachorro.com.br/wp-content/uploads/2017/01/cachorro-foto-de-divulga%C3%A7%C3%A3o.jpg',
-          titulo: 'Dog'
+    export default {
+        data() {
+            return {
+                fotos: []
+            }
         },
-        {
-          url: 'http://www.maedecachorro.com.br/wp-content/uploads/2017/01/cachorro-foto-de-divulga%C3%A7%C3%A3o.jpg',
-          titulo: 'Dogao'
+        created() {
+            this.$http.get('http://localhost:3000/v1/fotos')
+                .then(resp => resp.json());
+                .then(fotos => this.fotos = fotos, err => console.log(err));
         }
-      ]
     }
-  }
-}
 </script>
 
 <style>
