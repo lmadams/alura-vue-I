3869bb48ac67efedcb1dc590af5f958604b67a9c
Lucas Adams <adams.lucass@gmail.com>
2017-06-07 22:05:02 -0300

    Curso 1 finalizado

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,44 @@
 <template>
-    <div>
-        <h1 v-text="titulo"></h1>
+    <div class="corpo">
 
-        <ul>
-            <li v-for="foto of fotos">
-                <img v-bind:src="foto.url" v-bind:alt="foto.titulo">
-            </li>
-        </ul>
+        <meu-menu :rotas="routes"></meu-menu>
+        <transition name="pagina">
+            <router-view></router-view>
+        </transition>
     </div>
 </template>
 
 <script>
+
+    import { routes } from './routes';
+    import Menu from './components/shared/menu/Menu.vue';
+
     export default {
+        components: {
+            'meu-menu': Menu
+        },
+
         data() {
             return {
-                fotos: []
+                routes
             }
-        },
-        created() {
-            this.$http.get('http://localhost:3000/v1/fotos')
-                .then(resp => resp.json());
-                .then(fotos => this.fotos = fotos, err => console.log(err));
         }
     }
 </script>
 
-<style>
+<style scoped>
+    .corpo {
+        font-family: Helvetica, sans-serif;
+        width: 96%;
+        margin: 0 auto;
+    }
+
+    .pagina-enter, .pagina-leave-active {
+        opacity: 0
+    }
+
+    .pagina-enter-active, .pagina-leave-active {
+        transition: opacity .4s
+    }
 
 </style>
